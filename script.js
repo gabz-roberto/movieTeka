66167924b569d0527cc6a48207a9b9e3540d7b37
Gabriel Roberto <gabrielrobertoal@gmail.com>
2021-07-06 14:05:54 -0300

    slider

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ $(document).ready(() => {
         console.log($('#nav-menu'))
     })
 })
+
+// Owl Carousel config
+let navText = [
+    "<i class='bx bx-chevron-left'></i>",
+    "<i class='bx bx-chevron-right'></i>",
+]
+
+$('#hero-carousel').owlCarousel({
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: navText,
+    autoplayHoverPause: true
+})
