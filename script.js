b6ed0c070c2b4794ff0c64617721d0f6f17e9a9a
Gabriel Roberto <gabrielrobertoal@gmail.com>
2021-07-07 14:47:21 -0300

    sections

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
 // Toggle animation
 $(document).ready(() => {
-    $('#hamburger-menu').click(() => {
-        $('#hamburger-menu').toggleClass('active');
-        $('#nav-menu').toggleClass('active')
-        console.log($('#nav-menu'))
-    })
-})
-
-// Owl Carousel config
-let navText = [
+  $("#hamburger-menu").click(() => {
+    $("#hamburger-menu").toggleClass("active");
+    $("#nav-menu").toggleClass("active");
+    console.log($("#nav-menu"));
+  });
+  // Owl Carousel config
+  let navText = [
     "<i class='bx bx-chevron-left'></i>",
     "<i class='bx bx-chevron-right'></i>",
-]
+  ];
 
-$('#hero-carousel').owlCarousel({
+  $("#hero-carousel").owlCarousel({
     items: 1,
     dots: false,
     loop: true,
     nav: true,
     navText: navText,
-    autoplayHoverPause: true
-})
+    autoplay: true,
+    autoplayHoverPause: true,
+  });
+
+  $("#top-movies-slide").owlCarousel({
+    items: 2,
+    dots: false,
+    loop: true,
+    autoplay: false,
+    autoplayHoverPause: true,
+    responsive: {
+      500: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      },
+      1600: {
+        items: 6
+      },
+    }
+  });
+
+  $('.movies-slide').owlCarousel({
+    items: 2,
+    dots: false,
+    nav: true,
+    navText: navText,
+    margin: 10,
+    responsive: {
+      500: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      },
+      1600: {
+        items: 6
+      },
+    }
+  })
+});
